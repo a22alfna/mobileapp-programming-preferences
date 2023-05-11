2f999cd1c032956fe0dbf4435599c8926150f04d
a22alfna <a22alfna@student.his.se>
2023-05-11 14:12:20 +0200

    Jag gav min text view ett ID som jag döpte till name och så fixade jag min MainActivity och dess variabler och funktioner.

--- app/src/main/java/com/example/project/MainActivity.java
+++ app/src/main/java/com/example/project/MainActivity.java
@@ -2,17 +2,35 @@ package com.example.project;
 
 import android.os.Bundle;
 
+import android.content.SharedPreferences;
+import android.widget.TextView;
+
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.appcompat.widget.Toolbar;
 
 public class MainActivity extends AppCompatActivity {
 
+    TextView text;
+    SharedPreferences preferences;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         Toolbar toolbar = findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
+        text = findViewById(R.id.name);
+
+        preferences = getSharedPreferences("preferences", MODE_PRIVATE);
+        SharedPreferences.Editor editor = preferences.edit();
+        editor.putString("name", "Alfred");
+        editor.apply();
+    }
+    @Override
+    protected void onResume() {
+        super.onResume();
+
+        String name = preferences.getString("name", "inget namn hittades");
+        text.setText(name);
     }
 
 }
