a1eebaf95caffa059e53f1063fc87f4c67fd8d69
a22alfna <a22alfna@student.his.se>
2023-05-12 14:01:25 +0200

    Jag lade till lite olika variabler och funktioner för att bland annat kunna gå tillbaka till första sidan när man väl skrivit in sina preferences i SecondActivity

--- app/src/main/java/com/example/project/SecondActivity.java
+++ app/src/main/java/com/example/project/SecondActivity.java
@@ -1,5 +1,44 @@
 package com.example.project;
 
-public class SecondActivity {
+import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
+import android.content.SharedPreferences;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+
+public class SecondActivity extends AppCompatActivity {
+
+    Button tillbaka;
+    EditText namn;
+    SharedPreferences preferences;
+    Button button;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_second);
+
+        namn = findViewById(R.id.redigera);
+        button = findViewById(R.id.spara);
+        preferences = getSharedPreferences("preferences", MODE_PRIVATE);
+        tillbaka = findViewById(R.id.tillbaka);
+        tillbaka.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                finish();
+            }
+        });
+
+        button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                SharedPreferences.Editor editor = preferences.edit();
+                editor.putString("name", namn.getText().toString());
+                editor.apply();
+            }
+        });
+    }
 }
